559b4eee1d61e1144602abc6a0d3dcfe0b9c1f62
Jared Atchison <jared@jaredatchison.com>
2013-07-17 15:47:40 -0500

    Add front-page.php, addresses #22

---
+++ front-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * EA Genesis Child.
+ *
+ * @package      EAGenesisChild
+ * @since        1.0.0
+ * @copyright    Copyright (c) 2013, Contributors to EA Genesis Child project
+ * @license      GPL-2.0+
+ */
+
+// Full width layout
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+// Remove post title area
+remove_action( 'genesis_entry_header', 'genesis_do_post_title'                 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open',  5  );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+genesis();
