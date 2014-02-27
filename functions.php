743d5d9e2d327e1f5cb05eb7b147ffca1035341e
Bill Erickson <bill.erickson@gmail.com>
2014-02-27 15:51:11 -0800

    dont update theme, updated to work with json, fixes #39

--- functions.php
+++ functions.php
@@ -51,6 +51,9 @@ function ea_child_theme_setup() {
 
 	// Remove metaboxes from home page
 	add_action( 'admin_menu', 'ea_home_remove_metaboxes', 50 );
+	
+	// Dont update theme
+	add_filter( 'http_request_args', 'ea_dont_update_theme', 5, 2 );
 
 	// Set comment area defaults
 	add_filter( 'comment_form_defaults', 'ea_comment_text' );
@@ -60,7 +63,7 @@ function ea_child_theme_setup() {
  
 	// Remove Header Description
 	remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
-        
+
 }
 add_action( 'genesis_setup', 'ea_child_theme_setup', 15 );
 
@@ -79,6 +82,29 @@ function ea_home_remove_metaboxes() {
 	}
 }
 
+/**
+ * Dont Update the Theme
+ *
+ * If there is a theme in the repo with the same name, this prevents WP from prompting an update.
+ *
+ * @since  1.0.0
+ * @author Bill Erickson
+ * @link   http://www.billerickson.net/excluding-theme-from-updates
+ * @param  array $r Existing request arguments
+ * @param  string $url Request URL
+ * @return array Amended request arguments
+ */
+function ea_dont_update_theme( $r, $url ) {
+	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) )
+ 		return $r; // Not a theme update request. Bail immediately.
+ 	$themes = json_decode( $r['body']['themes'] );
+ 	$child = get_option( 'stylesheet' );
+	unset( $themes->themes->$child );
+ 	$r['body']['themes'] = json_encode( $themes );
+ 	return $r;
+ }
+ 
+ 
 /**
  * Change the comment area text
  *
