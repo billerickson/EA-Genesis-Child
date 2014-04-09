5a1f4064f70cd482c479a8d4e9ba05bd392d17b4
Bill Erickson <bill.erickson@gmail.com>
2014-04-09 11:53:58 -0700

    equal height content and sidebar

--- js/global.js
+++ js/global.js
@@ -3,8 +3,16 @@ jQuery(document).ready(function($){
 	// FitVids
 	$('.entry-content').fitVids();
 
+	// Content and Sidebar, Equal Heights
+	var sidebarHeight = $(".sidebar-primary").outerHeight();
+	var bodyHeight = $(".content-sidebar-wrap > .content").outerHeight();
+	
+	if (bodyHeight > sidebarHeight) {
+		$(".sidebar-primary").css( 'min-height', bodyHeight );
+	};
+	
 /*	
-	// Equal Heights
+	// Footer Widgets, Equal Heights
 	function footer_widget_height() {
         if(window.innerWidth < 767) {
             $('.footer-widgets .widget-area').height('auto');
