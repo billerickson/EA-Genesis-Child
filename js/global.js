51d519e13ed61720af80517b88a1a30106457093
Bill Erickson <bill.erickson@gmail.com>
2014-10-09 11:53:39 -0700

    smooth scrolling anchor links, fixes #72

--- js/global.js
+++ js/global.js
@@ -3,4 +3,22 @@ jQuery(document).ready(function($){
 	// FitVids
 	$('.entry-content').fitVids();
 
+	// Smooth scrolling anchor links
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				var top_offset = 0;
+				if ( $('.site-header').css('position') == 'fixed' ) {
+					top_offset = $('.site-header').height();
+				}
+				 $('html,body').animate({
+					 scrollTop: target.offset().top - top_offset
+				}, 1000);
+				return false;
+			}
+		}
+	});
+	
 });
