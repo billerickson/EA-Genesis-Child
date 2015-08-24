5bb65354ecd1d495af97a2bb72845e115a3a8d28
Bill Erickson <bill.erickson@gmail.com>
2015-08-24 12:17:19 -0500

    remove genesis favicon, use WP site icon instead

--- inc/genesis-changes.php
+++ inc/genesis-changes.php
@@ -18,6 +18,9 @@ add_theme_support( 'genesis-menus', array( 'primary' => 'Primary Navigation Menu
 // Remove Edit link
 add_filter( 'genesis_edit_post_link', '__return_false' );
 
+// Remove Genesis Favicon (use site icon instead)
+remove_action( 'wp_head', 'genesis_load_favicon' );
+
 // Remove Header Description
 remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
 
