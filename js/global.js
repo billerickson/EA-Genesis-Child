90cafa3c7c489d8e28ea6d8b1dc8fa1cc7603770
Bill Erickson <bill.erickson@gmail.com>
2015-09-16 15:13:36 -0500

    smooth scroll anchor links on click or pageload

--- js/global.js
+++ js/global.js
@@ -3,25 +3,35 @@ jQuery(function($){
 	// FitVids
 	$('.entry-content').fitVids();
 
+	
 	// Smooth scrolling anchor links
+	function ea_scroll( hash ) {
+		var target = $( hash );
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+			var top_offset = 0;
+			if ( $('.site-header').css('position') == 'fixed' ) {
+				top_offset = $('.site-header').height();
+			}
+			if( $('body').hasClass('admin-bar') ) {
+				top_offset = top_offset + $('#wpadminbar').height();
+			}
+			 $('html,body').animate({
+				 scrollTop: target.offset().top - top_offset
+			}, 1000);
+			return false;
+		}
+	}
+	// -- Smooth scroll on pageload
+	if( window.location.hash ) {
+		ea_scroll( window.location.hash );
+	}
+	// -- Smooth scroll on click
 	$('a[href*=#]:not([href=#]):not(.no-scroll)').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				var top_offset = 0;
-				if ( $('.site-header').css('position') == 'fixed' ) {
-					top_offset = $('.site-header').height();
-				}
-				if( $('body').hasClass('admin-bar') ) {
-					top_offset = $('#wpadminbar').height();
-				}
-				 $('html,body').animate({
-					 scrollTop: target.offset().top - top_offset
-				}, 1000);
-				return false;
-			}
+			ea_scroll( this.hash );
 		}
 	});
+	
 	
 });
