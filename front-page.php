72ea32e1a98626dc62bf01739383010096e11b35
Bill Erickson <bill.erickson@gmail.com>
2015-09-22 15:01:35 -0500

    front page h1 was fixed in Genesis 2.2

--- front-page.php
+++ front-page.php
@@ -16,16 +16,4 @@ remove_action( 'genesis_entry_header', 'genesis_do_post_title'                 )
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open',  5  );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
 
-/**
- * Use h1 for site title
- *
- */
-function ea_front_page_h1( $title, $inside, $wrap ){
-	$wrap = 'h1';
-	$title  = genesis_html5() ? sprintf( "<{$wrap} %s>", genesis_attr( 'site-title' ) ) : sprintf( '<%s id="title">%s</%s>', $wrap, $inside, $wrap );
-	$title .= genesis_html5() ? "{$inside}</{$wrap}>" : '';
-	return $title;
-}
-add_filter( 'genesis_seo_title', 'ea_front_page_h1', 10, 3 );
-
 genesis();
