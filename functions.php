4d230b58ac5a8af1b2054834c3209cc118f105b1
Bill Erickson <bill.erickson@gmail.com>
2018-02-27 14:49:29 -0600

    update styles

--- functions.php
+++ functions.php
@@ -90,6 +90,15 @@ function ea_global_enqueues() {
 add_action( 'wp_enqueue_scripts', 'ea_global_enqueues' );
 
 /**
+ * Gutenberg scripts and styles
+ *
+ */
+function ea_gutenberg_scripts() {
+	wp_enqueue_style( 'ea', get_stylesheet_directory_uri() . '/assets/css/gutenberg.css', array(), filemtime( get_stylesheet_directory() . '/assets/css/gutenberg.css' ) );
+}
+add_action( 'enqueue_block_editor_assets', 'ea_gutenberg_scripts' );
+
+/**
  * Blog Template
  *
  */
