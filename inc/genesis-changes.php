af240e036d1cc26c7687a9606693b6046fd46cb4
Bill Erickson <bill.erickson@gmail.com>
2018-02-27 15:18:52 -0600

    nav menu class

--- inc/genesis-changes.php
+++ inc/genesis-changes.php
@@ -12,7 +12,7 @@
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery' ) );
 add_theme_support( 'genesis-responsive-viewport' );
 add_theme_support( 'genesis-footer-widgets', 3 );
-add_theme_support( 'genesis-structural-wraps', array( 'header', 'menu-primary', 'menu-secondary', 'site-inner', 'footer-widgets', 'footer' ) );
+add_theme_support( 'genesis-structural-wraps', array( 'header', 'menu-secondary', 'site-inner', 'footer-widgets', 'footer' ) );
 add_theme_support( 'genesis-menus', array( 'primary' => 'Primary Navigation Menu', 'secondary' => 'Secondary Navigation Menu', 'mobile' => 'Mobile Menu' ) );
 add_theme_support( 'genesis-inpost-layouts' );
 add_theme_support( 'genesis-archive-layouts' );
@@ -270,14 +270,15 @@ function ea_default_term_description( $value, $term_id, $meta_key, $single ) {
 add_filter( 'get_term_metadata', 'ea_default_term_description', 10, 4 );
 
 /**
- * Nav Menu Class
+ * Add '.nav-menu' class to nav menus
  *
+ * @param string $open, opening markup
+ * @param array $args, markup args
+ * @return string
  */
-function ea_nav_menu_class( $args ) {
-
-	if( !empty( $args['menu_class'] ) )
-		$args['menu_class'] = str_replace( 'genesis-nav-menu', 'nav-menu', $args['menu_class'] );
-		
-	return $args;
+function ea_nav_menu_class( $open, $args ) {
+	$open = str_replace( $args['context'], $args['context'] . ' nav-menu', $open );
+	return $open;
 }
-add_filter( 'wp_nav_menu_args', 'ea_nav_menu_class' );
+add_filter( 'genesis_markup_nav-primary_open', 'ea_nav_menu_class', 10, 2 );
+add_filter( 'genesis_markup_nav-secondary_open', 'ea_nav_menu_class', 10, 2 );
