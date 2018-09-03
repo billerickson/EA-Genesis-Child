79f528592ceaf7cf42eee507b65822c78ed420c4
Bill Erickson <bill.erickson@gmail.com>
2018-09-03 11:53:57 -0500

    html5 caption, remove admin bar styling

--- inc/genesis-changes.php
+++ inc/genesis-changes.php
@@ -9,13 +9,16 @@
 **/
 
 // Theme Supports
-add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery' ) );
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
 add_theme_support( 'genesis-responsive-viewport' );
 add_theme_support( 'genesis-footer-widgets', 3 );
 add_theme_support( 'genesis-structural-wraps', array( 'header', 'menu-secondary', 'site-inner', 'footer-widgets', 'footer' ) );
 add_theme_support( 'genesis-menus', array( 'primary' => 'Primary Navigation Menu', 'secondary' => 'Secondary Navigation Menu', 'mobile' => 'Mobile Menu' ) );
 add_theme_support( 'genesis-inpost-layouts' );
 add_theme_support( 'genesis-archive-layouts' );
+
+// Remove admin bar styling
+add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
 
 // Remove Edit link
 add_filter( 'genesis_edit_post_link', '__return_false' );
