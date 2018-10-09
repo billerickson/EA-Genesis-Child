d3b69dc01f63125cc5117ba5a83db89da8d95873
Bill Erickson <bill.erickson@gmail.com>
2018-10-09 08:33:56 -0500

    accessibility improvements
    see: https://make.wordpress.org/accessibility/handbook/markup/post-excerpts-for-an-archive-template/

--- partials/archive.php
+++ partials/archive.php
@@ -2,7 +2,7 @@
 /**
  * Archive partial
  *
- * @package      EAGenesisChild
+ * @package      EAStarter
  * @author       Bill Erickson
  * @since        1.0.0
  * @license      GPL-2.0+
@@ -10,12 +10,15 @@
 
 echo '<article class="post-summary">';
 
+	echo '<a class="entry-image-link" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . get_the_post_thumbnail( get_the_ID(), 'medium' ) . '</a>';
+
 	echo '<header class="entry-header">';
 		echo '<h2 class="entry-title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
 	echo '</header>';
 
 	echo '<div class="entry-content">';
 		the_excerpt();
+		echo '<p><a class="read-more" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">Read More<span class="screen-reader-text"> of ' . get_the_title() . '</span></a></p>';
 	echo '</div>';
 
 echo '</article>';
