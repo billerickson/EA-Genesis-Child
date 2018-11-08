98f0c23563046a3faaf4348647fbcbb2d581675e
Bill Erickson <bill.erickson@gmail.com>
2018-11-08 13:49:16 -0600

    add code to disable editor, not currently used

--- functions.php
+++ functions.php
@@ -38,6 +38,7 @@ function ea_child_theme_setup() {
 	include_once( get_stylesheet_directory() . '/inc/genesis-changes.php' );
 	include_once( get_stylesheet_directory() . '/inc/login-logo.php' );
     include_once( get_stylesheet_directory() . '/inc/tinymce.php' );
+	include_once( get_stylesheet_directory() . '/inc/disable-editor.php' );
 	include_once( get_stylesheet_directory() . '/inc/helper-functions.php' );
 	include_once( get_stylesheet_directory() . '/inc/navigation.php' );
 	include_once( get_stylesheet_directory() . '/inc/loop.php' );
---
+++ inc/disable-editor.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Disable Editor
+ *
+ * @package      EAGenesisChild
+ * @author       Bill Erickson
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
+
+/**
+ * Templates and Page IDs without editor
+ *
+ */
+function ea_disable_editor( $id = false ) {
+
+	$excluded_templates = array(
+	);
+
+	$excluded_ids = array(
+	);
+
+	if( empty( $id ) )
+		return false;
+
+	$id = intval( $id );
+	$template = get_page_template_slug( $id );
+
+	return in_array( $id, $excluded_ids ) || in_array( $template, $excluded_templates );
+}
+
+/**
+ * Disable Gutenberg by template
+ *
+ */
+function ea_disable_gutenberg( $can_edit, $post_type ) {
+
+	if( ! ( is_admin() && !empty( $_GET['post'] ) ) )
+		return $can_edit;
+
+	if( ea_disable_editor( $_GET['post'] ) )
+		$can_edit = false;
+
+	return $can_edit;
+
+}
+add_filter( 'gutenberg_can_edit_post_type', 'ea_disable_gutenberg', 10, 2 );
+add_filter( 'use_block_editor_for_post_type', 'ea_disable_gutenberg', 10, 2 );
+
+/**
+ * Disable Classic Editor by template
+ *
+ */
+function ea_disable_classic_editor() {
+
+	$screen = get_current_screen();
+	if( 'page' !== $screen->id || ! isset( $_GET['post']) )
+		return;
+
+	if( ea_disable_editor( $_GET['post'] ) ) {
+		remove_post_type_support( 'page', 'editor' );
+	}
+
+}
+add_action( 'admin_head', 'ea_disable_classic_editor' );
