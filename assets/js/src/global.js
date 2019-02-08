3a3ada55e1da663749b1c329d79b04916f5a213f
Bill Erickson <bill.erickson@gmail.com>
2019-02-08 14:36:54 -0600

    remove sidr

--- assets/js/src/global.js
+++ assets/js/src/global.js
@@ -1,36 +1,9 @@
 // @codekit-prepend "jquery.fitvids.js"
-// @codekit-prepend "jquery.sidr.min.js"
 // @codekit-prepend "smoothscroll.js"
 
 jQuery(function($){
 
 	// FitVids
 	$('.entry-content').fitVids();
-
-	// Mobile Menu
-	$('.mobile-menu-toggle').sidr({
-		name: 'sidr-mobile-menu',
-		side: 'right',
-		renaming: false,
-		displace: false,
-	});
-	$('.menu-item-has-children').prepend('<span class="submenu-toggle">' );
-	$('.menu-item-has-children.sidr-class-current-menu-item').addClass('submenu-active');
-	$('.menu-item-has-children > .submenu-toggle').click(function(e){
-		$(this).parent().toggleClass('submenu-active');
-		e.preventDefault();
-	});
-	$('.sidr a').click(function(){
-		$.sidr('close', 'sidr-mobile-menu');
-	});
-	$('.sidr-menu-close').click(function(e){
-		e.preventDefault();
-	});
-	$(document).on( 'mouseup touchend', (function (e){
-		var container = $("#sidr-mobile-menu");
-		if (!container.is(e.target) && container.has(e.target).length === 0) {
-			$.sidr('close', 'sidr-mobile-menu');
-		}
-	}));
 
 });
--- inc/navigation.php
+++ inc/navigation.php
@@ -11,32 +11,3 @@
 // Primary Nav in Header
 remove_action( 'genesis_after_header', 'genesis_do_nav' );
 add_action( 'genesis_header', 'genesis_do_nav', 11 );
-
-/**
- * Mobile Menu Toggle
- *
- */
-function ea_mobile_menu_toggle() {
-
-	if( ! has_nav_menu( 'mobile' ) )
-		return;
-
-    echo '<div class="nav-mobile">';
-	echo '<a class="mobile-menu-toggle" href="#">' . ea_icon( 'menu' ) . '</a>';
-	echo '</div>';
-}
-add_action( 'genesis_header', 'ea_mobile_menu_toggle', 12 );
-
-/**
- * Mobile Menu
- *
- */
-function ea_mobile_menu() {
-  if( has_nav_menu( 'mobile' ) ) {
-    echo '<div id="sidr-mobile-menu" class="sidr right">';
-      echo '<a class="sidr-menu-close" href="#">' . ea_icon( 'close' ) . '</a>';
-      wp_nav_menu( array( 'theme_location' => 'mobile' ) );
-    echo '</div>';
-  }
-}
-add_action( 'wp_footer', 'ea_mobile_menu' );
