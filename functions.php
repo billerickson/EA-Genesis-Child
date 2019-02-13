c2da13b1b77cf12352116ebbe8dc5c46bbc94e06
Bill Erickson <bill.erickson@gmail.com>
2019-02-13 13:41:14 -0600

    rearrange functions.php to match ea-starter

--- functions.php
+++ functions.php
@@ -22,6 +22,54 @@ if ( ! isset( $content_width ) )
     $content_width = 1024;
 
 /**
+ * Global enqueues
+ *
+ * @since  1.0.0
+ * @global array $wp_styles
+ */
+function ea_global_enqueues() {
+
+	// javascript
+	wp_enqueue_script( 'ea-global', get_stylesheet_directory_uri() . '/assets/js/global-min.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/assets/js/global-min.js' ), true );
+
+	// css
+    wp_dequeue_style( 'child-theme' );
+	wp_enqueue_style( 'ea-fonts', ea_theme_fonts_url() );
+    wp_enqueue_style( 'ea-style', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), CHILD_THEME_VERSION );
+
+	// Move jQuery to footer
+	if( ! is_admin() ) {
+		wp_deregister_script( 'jquery' );
+		wp_register_script( 'jquery', includes_url( '/js/jquery/jquery.js' ), false, NULL, true );
+		wp_enqueue_script( 'jquery' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'ea_global_enqueues' );
+
+/**
+ * Gutenberg scripts and styles
+ *
+ */
+function ea_gutenberg_scripts() {
+	wp_enqueue_style( 'ea-fonts', ea_theme_fonts_url() );
+}
+add_action( 'enqueue_block_editor_assets', 'ea_gutenberg_scripts' );
+
+/**
+ * Theme Fonts URL
+ *
+ */
+function ea_theme_fonts_url() {
+	$font_families = apply_filters( 'ea_theme_fonts', array( 'Source+Sans+Pro:400,400i,700,700i' ) );
+	$query_args = array(
+		'family' => implode( '|', $font_families ),
+		'subset' => 'latin,latin-ext',
+	);
+	$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	return esc_url_raw( $fonts_url );
+}
+
+/**
  * Theme setup.
  *
  * Attach all of the site-wide functions to the correct hooks and filters. All
@@ -130,55 +178,6 @@ function ea_comment_text( $args ) {
 	return $args;
 }
 add_filter( 'comment_form_defaults', 'ea_comment_text' );
-
-/**
- * Global enqueues
- *
- * @since  1.0.0
- * @global array $wp_styles
- */
-function ea_global_enqueues() {
-
-	// javascript
-	wp_enqueue_script( 'ea-global', get_stylesheet_directory_uri() . '/assets/js/global-min.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/assets/js/global-min.js' ), true );
-
-	// css
-    wp_dequeue_style( 'child-theme' );
-	wp_enqueue_style( 'ea-fonts', ea_theme_fonts_url() );
-    wp_enqueue_style( 'ea-style', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), CHILD_THEME_VERSION );
-
-	// Move jQuery to footer
-	if( ! is_admin() ) {
-		wp_deregister_script( 'jquery' );
-		wp_register_script( 'jquery', includes_url( '/js/jquery/jquery.js' ), false, NULL, true );
-		wp_enqueue_script( 'jquery' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'ea_global_enqueues' );
-
-/**
- * Gutenberg scripts and styles
- *
- */
-function ea_gutenberg_scripts() {
-	wp_enqueue_style( 'ea-fonts', ea_theme_fonts_url() );
-}
-add_action( 'enqueue_block_editor_assets', 'ea_gutenberg_scripts' );
-
-
-/**
- * Theme Fonts URL
- *
- */
-function ea_theme_fonts_url() {
-	$font_families = apply_filters( 'ea_theme_fonts', array( 'Source+Sans+Pro:400,400i,700,700i' ) );
-	$query_args = array(
-		'family' => implode( '|', $font_families ),
-		'subset' => 'latin,latin-ext',
-	);
-	$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
-	return esc_url_raw( $fonts_url );
-}
 
 /**
  * Template Hierarchy
