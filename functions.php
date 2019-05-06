d0ab555ac28cbad801083c775549788d3dd70891
Bill Erickson <bill.erickson@gmail.com>
2019-05-06 11:45:57 -0500

    amp helper functions

--- functions.php
+++ functions.php
@@ -93,6 +93,7 @@ function ea_child_theme_setup() {
 	include_once( get_stylesheet_directory() . '/inc/helper-functions.php' );
 	include_once( get_stylesheet_directory() . '/inc/navigation.php' );
 	include_once( get_stylesheet_directory() . '/inc/loop.php' );
+	include_once( get_stylesheet_directory() . '/inc/amp.php' );
 	//include_once( get_stylesheet_directory() . '/inc/custom-logo.php' );
 
 	// Editor Styles
---
+++ inc/amp.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * AMP functionality
+ *
+ * @package      EAStarter
+ * @author       Bill Erickson
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
+
+/**
+ * Is AMP?
+ * Conditional tag
+ */
+function ea_is_amp() {
+	return function_exists( 'is_amp_endpoint' ) && is_amp_endpoint();
+}
+
+/**
+ * AMP Class
+ *
+ */
+function ea_amp_class( $default, $active, $variable ) {
+	$output = '';
+	if( ea_is_amp() ) {
+		$output .= ' [class]="' . $variable . ' ? \'' . $default . ' ' . $active . '\' : \'' . $default . '\'"';
+	}
+	$output .= ' class="' . $default . '"';
+	return $output;
+}
+
+/**
+ * AMP Toggle
+ *
+ */
+function ea_amp_toggle( $variable ) {
+	return ea_is_amp() ? ' on="tap:AMP.setState({' . $variable . ': !' . $variable . '})"' : '';
+}
+
+/**
+ * AMP Nav Dropdown
+ *
+ */
+function ea_amp_nav_dropdown( $theme_location = false, $depth = 0 ) {
+
+	$key = 'nav';
+	if( !empty( $theme_location ) )
+		$key .= ucwords( $theme_location );
+
+	global $submenu_index;
+	$submenu_index++;
+	$key .= 'SubmenuExpanded' . $submenu_index;
+
+	if( 1 < $depth )
+		$key .= 'Depth' . $depth;
+
+	return ea_amp_toggle( $key ) . ea_amp_class( 'submenu-expand', 'expanded', $key );
+}
