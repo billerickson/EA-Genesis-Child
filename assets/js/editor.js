8cdfb111ce39a26c0e23bcec9211b8110a44c4a6
Bill Erickson <bill.erickson@gmail.com>
2019-05-27 14:52:39 -0500

    block styles

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -2,4 +2,23 @@ wp.domReady( () => {
 	wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
+
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'wide' );
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'dots' );
+
+	wp.blocks.unregisterBlockStyle( 'core/quote', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
+
+	wp.blocks.registerBlockStyle( 'core/button', {
+		name: 'default',
+		label: 'Default',
+		isDefault: true,
+	});
+
+	wp.blocks.registerBlockStyle( 'core/button', {
+		name: 'full',
+		label: 'Full Width',
+	});
+
 } );
