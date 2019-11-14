5f7338390529789f3a5a8d5798c0727b9f9d60bd
Bill Erickson <bill.erickson@gmail.com>
2019-11-14 14:30:37 -0600

    amp compatible nav

--- assets/js/src/global.js
+++ assets/js/src/global.js
@@ -1,12 +1,20 @@
 jQuery(function($){
 
 	// Mobile Menu
-	$('.mobile-menu-toggle').click(function(){
-		$('.mobile-menu-toggle, .nav-primary').toggleClass('active');
+	$('.menu-toggle').click(function(){
+		$('.search-toggle, .search-wrap').removeClass('active');
+		$('.menu-toggle, .nav-primary').toggleClass('active');
 	});
 	$('.menu-item-has-children > .submenu-expand').click(function(e){
 		$(this).toggleClass('expanded');
 		e.preventDefault();
+	});
+
+	// Search toggle
+	$('.search-toggle').click(function(){
+		$('.menu-toggle, .nav-primary').removeClass('active');
+		$('.search-toggle, .header-search').toggleClass('active');
+		$('.site-header .search-field').focus();
 	});
 
 });
--- inc/navigation.php
+++ inc/navigation.php
@@ -8,24 +8,53 @@
  * @license      GPL-2.0+
 **/
 
-// Primary Nav in Header
+// Don't let Genesis load menus
 remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_header', 'genesis_do_nav', 12 );
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
 
 /**
  * Mobile Menu
  *
  */
-function ea_mobile_menu() {
-	echo '<nav class="nav-mobile">';
-		echo '<button class="mobile-menu-toggle">';
-			echo ea_icon( array( 'icon' => 'menu', 'size' => 14, 'class' => 'menu-open' ) );
-			echo ea_icon( array( 'icon' => 'close', 'size' => 14, 'class' => 'menu-close' ) );
-			echo '<span class="screen-reader-text">Menu</span>';
-		echo '</button>';
-	echo '</nav>';
+function ea_site_header() {
+	echo ea_mobile_menu_toggle();
+	echo ea_search_toggle();
+
+	if( has_nav_menu( 'primary' ) ) {
+		echo '<nav' . ea_amp_class( 'nav-primary nav-menu', 'active', 'menuActive' ) . ' role="navigation">';
+		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) );
+		echo '</nav>';
+	}
+
+	echo '<div' . ea_amp_class( 'header-search', 'active', 'searchActive' ) . '>' . get_search_form( array( 'echo' => false ) ) . '</div>';
 }
-add_action( 'genesis_header', 'ea_mobile_menu', 11 );
+add_action( 'genesis_header', 'ea_site_header', 11 );
+
+/**
+ * Search toggle
+ *
+ */
+function ea_search_toggle() {
+	$output = '<button' . ea_amp_class( 'search-toggle', 'active', 'searchActive' ) . ea_amp_toggle( 'searchActive', array( 'menuActive', 'mobileFollow' ) ) . '>';
+		$output .= ea_icon( array( 'icon' => 'search', 'size' => 24, 'class' => 'open' ) );
+		$output .= ea_icon( array( 'icon' => 'close', 'size' => 24, 'class' => 'close' ) );
+		$output .= '<span class="screen-reader-text">Search</span>';
+	$output .= '</button>';
+	return $output;
+}
+
+/**
+ * Mobile menu toggle
+ *
+ */
+function ea_mobile_menu_toggle() {
+	$output = '<button' . ea_amp_class( 'menu-toggle', 'active', 'menuActive' ) . ea_amp_toggle( 'menuActive', array( 'searchActive', 'mobileFollow' ) ) . '>';
+		$output .= ea_icon( array( 'icon' => 'menu', 'size' => 24, 'class' => 'open' ) );
+		$output .= ea_icon( array( 'icon' => 'close', 'size' => 24, 'class' => 'close' ) );
+		$output .= '<span class="screen-reader-text">Menu</span>';
+	$output .= '</button>';
+	return $output;
+}
 
 /**
  * Add a dropdown icon to top-level menu items.
