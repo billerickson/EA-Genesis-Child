8f1612b10d6aee454f20435df880ca094b2f3c51
Bill Erickson <bill.erickson@gmail.com>
2019-12-06 07:05:22 -0600

    remove newsletter and related posts

--- single.php
+++ single.php
@@ -39,29 +39,6 @@ function ea_single_after_entry() {
 
 	// Author Box
 	genesis_do_author_box_single();
-
-	// Newsletter signup
-	$form_id = get_option( 'options_ea_newsletter_form' );
-	if( $form_id && function_exists( 'wpforms_display' ) )
-		wpforms_display( $form_id, true, true );
-
-	// Related Posts
-	$loop = new WP_Query( [
-		'posts_per_page'	=> 3,
-		'post__not_in'		=> [ get_the_ID() ],
-		'category_name'		=> ea_first_term( [ 'field' => 'slug' ] ),
-	] );
-	if( $loop->have_posts() ):
-		echo '<section class="post-listing-block layout3">';
-		echo '<header><h2>Other articles you may like</h2></header>';
-		while( $loop->have_posts() ):
-			$loop->the_post();
-			get_template_part( 'partials/archive' );
-		endwhile;
-		echo '</section>';
-		wp_reset_postdata();
-	endif;
-	echo '</div>';
 }
 add_action( 'genesis_after_entry', 'ea_single_after_entry', 8 );
 remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
